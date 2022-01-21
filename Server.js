b62f064449556628f5fcbea0d8816b9fc7f477ae
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-01-21 11:48:15 -0500

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,10 @@
+//server.js
+const http = require('http'),
+server = http.createServer((request,response)=>{
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.write('Hello world');
+    response.end();
+});
+server.listen(3000,()=>{
+   console.log('Node server created at port 3000');
+});
